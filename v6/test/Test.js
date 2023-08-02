f9297aca4539cfb9b43327818e4d6fc1640deec9
JustynaBroniszewska <jusbroniszewska@gmail.com>
2023-08-02 11:35:56 +0200

    🐵 Upgrade provider-ganache

--- v6/test/Test.js
+++ v6/test/Test.js
@@ -3,18 +3,16 @@ const {defaultAccounts} = require('./utils.js')
 const { Wallet, ContractFactory } = require('ethers')
 const { GanacheProvider } = require('@ethers-ext/provider-ganache')
 
-
 describe('Test', () => {
   it('fails', async () => {
-    const provider = new GanacheProvider()
+    const provider = new GanacheProvider({ logging: false })
     
     const wallet = new Wallet(defaultAccounts[0].secretKey, provider)
     
-    await provider.setAccount(wallet.address, defaultAccounts[0].balance)
+    await provider.setAccount(wallet.address, { balance: defaultAccounts[0].balance })
     const factory = new ContractFactory(TestContract.abi, TestContract.bytecode, wallet)
     const testContract = await factory.deploy()
-    // const tx = await testContract.test({ gasLimit: 999999 })
-    // await tx.wait()
+    const tx = await testContract.test({ gasLimit: 999999 })
+    await tx.wait()
   }).timeout(2000000)
-
 })
